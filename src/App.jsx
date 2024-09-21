114f53ed7bcf84828fd6cd0bd9dfa4a655e9758d
Vũ Xuân Trường <126589378+IamXuanTruong@users.noreply.github.com>
2024-09-21 10:10:21 +0700

    updatelibrary

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import {Layout} from "@components/Layout.jsx";
-import {Login} from "@/src/pages/Login.jsx";
+import { Layout } from "@components/Layout.jsx";
+import { Login } from "@/src/pages/Login.jsx";
 import { ProfilePage } from './pages/ProfilePage';
-import {TripDetail} from "@/src/pages/TripDetail.jsx";
+import { TripDetail } from "@/src/pages/TripDetail.jsx";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "@/src/pages/HomePage.jsx";
 import { ContactPage } from './pages/ContactPage';
 import { AboutPage } from './pages/AboutPage';
+import { LibraryPage } from "./pages/LibraryPage";
 
 const features = [
   {
@@ -23,16 +24,17 @@ const features = [
 ];
 
 const App = () => (
-    <Routes >
-        <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-        </Route>
-        <Route path="/auth/login" element={<Login />}/>
-      <Route path='/profile' element={<ProfilePage />} />
-      <Route path='/trip/:id' element={<TripDetail />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/about' element={<AboutPage />} />
-    </Routes>
+  <Routes >
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+    <Route path="/auth/login" element={<Login />} />
+    <Route path='/profile' element={<ProfilePage />} />
+    <Route path='/trip/:id' element={<TripDetail />} />
+    <Route path='/contact' element={<ContactPage />} />
+    <Route path='/about' element={<AboutPage />} />
+    <Route path='/library' element={<LibraryPage />} />
+  </Routes>
 );
 
 export default App;
