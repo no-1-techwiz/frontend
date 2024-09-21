777f517cbaf7202b4951196c0a482681ed211ce0
anonyDev <phuochh@nortstudio.vn>
2024-09-21 12:02:55 +0700

    feat: merge library

---
+++ src/components/RecentlyViewed.jsx
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+
+export const RecentlyViewed = () => {
+
+
+    const [trip,setTrip] = useState()
+
+    useEffect(() => {
+
+    }, []);
+
+    return <div>
+
+    </div>
+}
