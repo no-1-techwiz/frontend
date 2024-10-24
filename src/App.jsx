d2a80a95efc5d37de25d59ce68b2b409042339f5
IamXuanTruong <126589378+IamXuanTruong@users.noreply.github.com>
2024-10-24 21:52:20 +0700

    create blog
    aaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import Feature from '@components/Feature';
 import Footer from '@components/Footer';
 import logo from '@images/logo.png';
-import {Route, Routes} from "react-router-dom";
-import {HomePage} from "@/src/pages/HomePage.jsx";
-import {Layout} from "@components/Layout.jsx";
-import {Login} from "@/src/pages/Login.jsx";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "@/src/pages/HomePage.jsx";
+import { Layout } from "@components/Layout.jsx";
+import { Login } from "@/src/pages/Login.jsx";
 import { ProfilePage } from './pages/ProfilePage';
-import {TripDetail} from "@/src/pages/TripDetail.jsx";
+import { TripDetail } from "@/src/pages/TripDetail.jsx";
 import { ContactPage } from './pages/ContactPage';
 import { AboutPage } from './pages/AboutPage';
 import { Admin } from './pages/admin/Admin';
-import {LibraryPage} from "@/src/pages/LibraryPage.jsx";
+import { LibraryPage } from "@/src/pages/LibraryPage.jsx";
 import { ListLocation } from './pages/admin/ListLocation';
 import { DetailsLocation } from './pages/admin/DetailsLocation';
 import { ListCurrencies } from './pages/admin/ListCurrencies';
+import BlogPage from './pages/BlogPage';
 
 const features = [
   {
@@ -31,25 +32,26 @@ const features = [
 ];
 
 const App = () => (
-    <Routes >
-        <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/library" element={<LibraryPage />} />
-            <Route path='/profile/:username' element={<ProfilePage />} />
-        <Route path='/contact' element={<ContactPage />} />
-            <Route path='/about' element={<AboutPage/>}/>
-        </Route>
-        <Route path="/auth/login" element={<Login />}/>
-      <Route path='/trip/:id' element={<TripDetail />} />
+  <Routes >
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/library" element={<LibraryPage />} />
+      <Route path='/profile/:username' element={<ProfilePage />} />
+      <Route path='/contact' element={<ContactPage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/blog' element={<BlogPage />} />
+    </Route>
+    <Route path="/auth/login" element={<Login />} />
+    <Route path='/trip/:id' element={<TripDetail />} />
 
-      <Route path='/admin' element={<Admin/>}>
-      <Route index element={<ListLocation/>}/>
-        <Route index path='/admin/location' element={<ListLocation/>}/>
-        <Route path='/admin/location/:id' element={<DetailsLocation/>}/>
-        <Route path='/admin/currency' element={<ListCurrencies  />}/>
-      </Route>
-    </Routes>
+    <Route path='/admin' element={<Admin />}>
+      <Route index element={<ListLocation />} />
+      <Route index path='/admin/location' element={<ListLocation />} />
+      <Route path='/admin/location/:id' element={<DetailsLocation />} />
+      <Route path='/admin/currency' element={<ListCurrencies />} />
+    </Route>
+  </Routes>
 );
 
 export default App;
